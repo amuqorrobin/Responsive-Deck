dbd47504c016d4f1321bf2a374dc389b784740ef
itsmemuqorrobin <achmadmuqorrobinassholih@gmail.com>
2026-03-11 12:07:41 +0700

    Project Preparation: Adjust the name and description of the project

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,6 +1,12 @@
-import { defineConfig } from 'wxt';
+import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  modules: ['@wxt-dev/module-react'],
+  modules: ["@wxt-dev/module-react"],
+  srcDir: "src",
+  manifest: {
+    name: "Responsive Deck",
+    description:
+      "Frontend Responsive Tools for Security Paranoid and Prominent User Experience",
+  },
 });
