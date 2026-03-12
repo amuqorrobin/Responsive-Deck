7b0a612bfa3d28ac78990deea34f9ad2082cfdbb
itsmemuqorrobin <achmadmuqorrobinassholih@gmail.com>
2026-03-12 12:10:16 +0700

    Project Preparation: install tailwind for styling

--- src/entrypoints/popup/App.tsx
+++ src/entrypoints/popup/App.tsx
@@ -1,34 +1,14 @@
 import { useState } from "react";
 
 import ResponsiveDeck from "@/assets/responsiveDeck.svg";
-import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://wxt.dev" target="_blank">
-          {/* <img src={wxtLogo} className="logo" alt="WXT logo" /> */}
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={ResponsiveDeck} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>WXT + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the WXT and React logos to learn more
-      </p>
-    </>
+    <section className="px-8 py-6 ">
+      <div className="text-blue-300">testing</div>
+    </section>
   );
 }
 
--- wxt.config.ts
+++ wxt.config.ts
@@ -1,3 +1,4 @@
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
@@ -9,4 +10,7 @@ export default defineConfig({
     description:
       "Frontend Responsive Tools for Security Paranoid and Prominent User Experience",
   },
+  vite: () => ({
+    plugins: [tailwindcss()],
+  }),
 });
