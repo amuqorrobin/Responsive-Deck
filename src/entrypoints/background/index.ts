bafddb3ebb4c24440b663e87ea4d8008e22de6fb
itsmemuqorrobin <achmadmuqorrobinassholih@gmail.com>
2026-03-16 01:33:18 +0700

    Service Worker: Create dynamic rule within DeclarativeNetRequest

--- src/entrypoints/background/index.ts
+++ src/entrypoints/background/index.ts
@@ -1,3 +1,46 @@
 export default defineBackground(() => {
-  console.log('Hello background!', { id: browser.runtime.id });
+  browser.runtime.onInstalled.addListener(async () => {
+    const RULE_ID = 1;
+
+    await browser.declarativeNetRequest.updateDynamicRules({
+      removeRuleIds: [RULE_ID],
+    });
+
+    await browser.declarativeNetRequest.updateDynamicRules({
+      addRules: [
+        {
+          id: RULE_ID,
+          priority: 1,
+          action: {
+            type: browser.declarativeNetRequest.RuleActionType.MODIFY_HEADERS,
+            responseHeaders: [
+              {
+                header: "x-frame-options",
+                operation: browser.declarativeNetRequest.HeaderOperation.REMOVE,
+              },
+              {
+                header: "content-security-policy",
+                operation: browser.declarativeNetRequest.HeaderOperation.REMOVE,
+              },
+              {
+                header: "content-security-policy-report-only",
+                operation: browser.declarativeNetRequest.HeaderOperation.REMOVE,
+              },
+              {
+                header: "frame-options",
+                operation: browser.declarativeNetRequest.HeaderOperation.REMOVE,
+              },
+            ],
+          },
+          condition: {
+            urlFilter: "*",
+            resourceTypes: [
+              browser.declarativeNetRequest.ResourceType.SUB_FRAME,
+            ],
+            initiatorDomains: [browser.runtime.id],
+          },
+        },
+      ],
+    });
+  });
 });
