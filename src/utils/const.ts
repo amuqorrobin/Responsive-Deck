92d08e737d2f9dd7ab0c8f90a8a73f76431a69df
itsmemuqorrobin <achmadmuqorrobinassholih@gmail.com>
2026-03-23 23:55:04 +0700

    Const: Add new Extensions Variables as source of truth

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -4,3 +4,7 @@ export const EXTENSION_MESSAGES = {
   DECK_SCROLL: "DECK_SCROLL",
   DECK_SET_SCROLL: "DECK_SET_SCROLL",
 };
+
+export const EXTENSION_VARIABLES = {
+  STORAGE_KEY: "targetDeckUrl",
+};
